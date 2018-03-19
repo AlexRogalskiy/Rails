790710973a1f21bec94df4411b2d1d02d62c9007
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-03-19 13:39:07 +0300

    Changeset to clients

--- datalistic/db/migrate/20180126000610_create_clients.rb
+++ datalistic/db/migrate/20180126000610_create_clients.rb
@@ -3,7 +3,7 @@ class CreateClients < ActiveRecord::Migration[5.1]
     create_table :clients do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.text :address, null: false
+      t.text 	:address, null: false
       t.string :email, null: false
       t.string :phone, null: false
       t.text :comments
